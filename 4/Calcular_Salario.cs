723b178babb51829a39cec0b2f1b701a37a4ed64
Aluno Fiar Logatti <aluno@fiar.local>
2018-03-02 20:59:57 -0300

    Atualizacao

---
+++ 4/Calcular_Salario.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    class Calcular_Salario
+    {
+        #region Propriedades
+
+        public double Fixo { get; set; }
+        public double Quantidade { get; set; }
+        public double Valor { get; set; }
+        public double Comissao { get; set; }
+
+
+
+        #endregion Propriedades
+
+        #region Metodos
+
+        public double Salario_Final()
+        {
+            
+            double comissao_fixa = Comissao * Quantidade;
+            double comissao_variavel = (Valor / 100) * 5;
+
+            double salario_final = comissao_fixa + comissao_variavel + Fixo;
+
+            return salario_final;
+        }
+
+
+        #endregion Metodos
+
+
+
+    }
+}
---
+++ 4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calcular_Salario calculo = new Calcular_Salario();
+
+
+            Console.WriteLine("Fixo...");
+            calculo.Fixo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Comissao...");
+            calculo.Comissao = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Quantidade...");
+            calculo.Quantidade = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Valor...");
+            calculo.Valor = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Total...");
+
+            Calcular(calculo);
+
+
+            Console.ReadKey();
+
+
+        }
+
+        public static void Calcular(Calcular_Salario calculo)
+        {
+            Console.WriteLine(calculo.Salario_Final());
+        }
+    }
+}
