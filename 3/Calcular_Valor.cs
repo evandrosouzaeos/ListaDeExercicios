1388e4e26cd339c4eb3b2ccccd5e380aa25f5bfd
Aluno Fiar Logatti <aluno@fiar.local>
2018-03-02 20:36:52 -0300

    Atualizacao

---
+++ 3/Calcular_Valor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    class Calcular_Valor
+    {
+
+        #region Propriedades
+
+        public double Custo { get; set; }
+        public double Distribuidor { get; set; }
+        public double Impostos { get; set; }
+
+
+        #endregion Propriedades
+
+
+        #region Metodos
+
+        public double Valor_Final()
+        {
+            double imposto = Custo + (Custo *(Impostos/100));
+            double distribuidor = (Distribuidor/100);
+
+            double final = (imposto + (imposto*(Distribuidor / 100)));
+
+            return final;
+        }
+
+        #endregion Metodos
+
+    }
+}
---
+++ 3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calcular_Valor calculo = new Calcular_Valor();
+
+
+            Console.WriteLine("Custo...");
+            calculo.Custo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Imposto");
+            calculo.Impostos = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Distribuidor");
+            calculo.Distribuidor = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("Total...");
+
+            Calcular(calculo);
+
+
+            Console.ReadKey();
+
+        }
+
+        public static void Calcular(Calcular_Valor calculo)
+        {
+            Console.WriteLine(calculo.Valor_Final());
+        }
+    }
+}
